6b651536e6f874c944bf3ff3089a39f32b748d9b
Ostap <ostapko2003o@gmail.com>
2022-09-09 12:38:25 +0300

    problem solved with pass

--- src/lesson_1_task_7/Dispatcher.java
+++ src/lesson_1_task_7/Dispatcher.java
@@ -10,7 +10,6 @@ import lesson_1_task_7.controllers.TriangleController;
 public class Dispatcher {
     public static void main(String[] args) {
         // TODO when shape dont equal error
-        // TODO add pass ability to simple view & vertical view
 
         Triangle triangle = new Triangle(5);
         Rectangle rec = new Rectangle(5);
@@ -19,21 +18,23 @@ public class Dispatcher {
         Rectangle rec2 = new Rectangle(6);
 
         Triangle triangle3 = new Triangle(3);
+        Rectangle rec3 = new Rectangle(4);
         Line line = new Line(10);
         View view = new View();
 
-        view.simpleRender(TriangleController.bottomLeftTriangle(triangle));
-        view.simpleRender(TriangleController.topLeftTriangle(triangle));
-        view.simpleRender(TriangleController.topRightTriangle(triangle));
-        view.simpleRender(TriangleController.bottomRightTriangle(triangle));
-        view.simpleRender(RectangleController.cube(rec));
-        view.simpleRender(LineController.line(line, "."));
-        view.simpleRender(LineController.line(line, " "));
+        view.simpleRender(0, TriangleController.bottomLeftTriangle(triangle));
+        view.simpleRender(5, TriangleController.topLeftTriangle(triangle));
+        view.simpleRender(0, TriangleController.topRightTriangle(triangle));
+        view.simpleRender(0, TriangleController.bottomRightTriangle(triangle));
+        view.simpleRender(0, RectangleController.cube(rec));
+        view.simpleRender(0, LineController.line(line, "."));
+        view.simpleRender(0, LineController.line(line, " "));
 
         view.inlineRender(5, TriangleController.bottomRightTriangle(triangle), TriangleController.bottomLeftTriangle(triangle));
         view.inlineRender(2, TriangleController.bottomRightTriangle(triangle), RectangleController.cube(rec), TriangleController.bottomLeftTriangle(triangle));
         view.inlineRender(1, TriangleController.bottomRightTriangle(triangle2), RectangleController.cube(rec2), TriangleController.bottomLeftTriangle(triangle2));
         view.inlineRender(7, TriangleController.bottomRightTriangle(triangle3), TriangleController.bottomLeftTriangle(triangle3));
+        view.simpleRender(8, RectangleController.cube(rec3));
 
         view.verticalRender(TriangleController.bottomLeftTriangle(triangle), TriangleController.topLeftTriangle(triangle));
         view.verticalRender(TriangleController.bottomLeftTriangle(triangle), RectangleController.cube(rec), TriangleController.topLeftTriangle(triangle));
--- src/lesson_1_task_7/View.java
+++ src/lesson_1_task_7/View.java
@@ -1,15 +1,26 @@
 package lesson_1_task_7;
 
 public class View {
-    void simpleRender(String figure) {
-        System.out.println(figure);
+    void simpleRender(int pass, String figure) {
+        String res_string = "";
+        String[] figure_arr = figure.split("\n");
+        for (int j = 0; j < figure_arr.length; j++) {
+            for (int i = 0; i < pass; i++) {
+                res_string += ' ';
+            }
+            res_string += figure_arr[j];
+            if (j + 1 != figure_arr.length) {
+                res_string += "\n";
+            }
+        }
+        System.out.println(res_string);
     }
 
     void inlineRender(int pass, String... args) {
         String res_string = "";
         int biggest_size = sizeBiggestFigure(args);
         for (int i = 0; i < biggest_size + 1; i++) {
-            for (int k = 0; k < pass; k++){
+            for (int k = 0; k < pass; k++) {
                 res_string += ' ';
             }
             for (String arg : args) {
@@ -30,9 +41,7 @@ public class View {
         System.out.println(res_string);
     }
 
-    private
-
-    int sizeBiggestFigure(String[] figures) {
+    private int sizeBiggestFigure(String[] figures) {
         int max = 0;
         for (String figure : figures) {
             String[] test = figure.split("\n");
