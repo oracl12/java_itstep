6d117b4a08dd0102e3dded35ba53d66df206c0b8
Ostap <ostapko2003o@gmail.com>
2022-09-09 18:28:32 +0300

    refactor

--- src/lesson_1_task_7/controllers/TriangleController.java
+++ src/lesson_1_task_7/controllers/TriangleController.java
@@ -3,8 +3,10 @@ package lesson_1_task_7.controllers;
 import lesson_1_task_7.models.Triangle;
 
 public class TriangleController {
+    static String result;
+
     public static String bottomLeftTriangle(Triangle figure) {
-        String result = "";
+        result = "";
         for (int i = 1; i <= figure.x; i++) {
             for (int j = 0; j < i; j++) {
                 result += '.';
@@ -15,7 +17,7 @@ public class TriangleController {
     }
 
     public static String topLeftTriangle(Triangle figure) {
-        String result = "";
+        result = "";
         for (int i = 1; i <= figure.x; i++) {
             for (int j = figure.x; j >= 1; j--) {
                 if (j >= i) {
@@ -29,7 +31,7 @@ public class TriangleController {
 
 
     public static String topRightTriangle(Triangle figure) {
-        String result = "";
+        result = "";
         for (int i = figure.x; i  > 0; i--){
             for (int j = figure.x - i; j > 0; j--){
                 result += ' ';
@@ -43,7 +45,7 @@ public class TriangleController {
     };
 
     public static String bottomRightTriangle(Triangle figure) {
-        String result = "";
+        result = "";
         for (int i = 1; i < figure.x + 1; i++){
             for (int j = figure.x - i; j > 0; j--){
                 result += ' ';
